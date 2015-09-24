dc6972f342243cc061b91c12aea4ed4cca15aedc
Misrab M. Faizullah-Khan <faizullah.misrab@gmail.com>
2015-09-24 21:47:46 +0800

    automatic commit

--- jarvis.js
+++ jarvis.js
@@ -1,50 +1,92 @@
 $(function() {
-    console.log('running jarvis...');
-
-
-    /*
-        speech
-    */
-    var recognition = new webkitSpeechRecognition();
-    recognition.lang = "en-GB";
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    recognition.onresult = function(event) { 
-      console.log(event) 
-    }
-    recognition.start();
-
-
-    /*
-        search bar
-    */
-
-    var GO_MUTEX = false;
-
-    // go handle
-    function go() {
-        if (GO_MUTEX) return;
-        GO_MUTEX = true;
-        console.log('go!');
+	console.log('running jarvis...');
 
 
 
-        GO_MUTEX = false;
-    }
+	var contentDiv = $('#contentDiv');
+	var interimDiv = $('#interimDiv');
+	function handleNewUserInput(text) {
+		contentDiv.prepend('<div>'+text+'</div>');
+	}
 
 
-    // enter button
-    $(document).keypress(function (e) {
-        if (e.which == 13) {
-            go()
-            return false;
-        }
-    });
 
-    $('#go').click(function(e) {
-        e.preventDefault();
-        go()
-    });
+	/*
+		speech
+	*/
+	var recognition = new webkitSpeechRecognition();
+	recognition.lang = "en-GB";
+	recognition.continuous = true;
+	recognition.interimResults = true;
+	// recognition.onresult = function(event) {
+	//   console.log(event.results);
+	// }
 
-    $('#go_input').focus();
+	recognition.onresult = function(event) {
+		var final_transcript = '';
+		var interim_transcript = '';
+
+		// console.log(event);
+		interimDiv.html('');
+
+		for (var i = event.resultIndex; i < event.results.length; ++i) {
+			if (event.results[i].isFinal) {
+				final_transcript += event.results[i][0].transcript;
+			}
+			else {
+				interim_transcript += event.results[i][0].transcript;
+				interimDiv.html(interim_transcript);
+			}
+		}
+
+
+		// handle final
+		var cleaned = final_transcript.trim();
+		if (cleaned !== '') {
+			handleNewUserInput(cleaned);
+		}
+
+		// console.log(interim_transcript);
+		
+		// final_transcript = capitalize(final_transcript);
+		// final_span.innerHTML = linebreak(final_transcript);
+		// interim_span.innerHTML = linebreak(interim_transcript);
+	}
+
+	
+	recognition.start();
+
+
+	/*
+		search bar
+	*/
+
+	var GO_MUTEX = false;
+
+	// go handle
+	function go() {
+		if (GO_MUTEX) return;
+		GO_MUTEX = true;
+		console.log('go!');
+
+
+
+		GO_MUTEX = false;
+	}
+
+
+	// enter button
+	$(document).keypress(function (e) {
+		if (e.which == 13) {
+			go()
+			return false;
+		}
+	});
+
+	$('#go').click(function(e) {
+		e.preventDefault();
+		go()
+	});
+
+	$('#go_input').focus();
 });
